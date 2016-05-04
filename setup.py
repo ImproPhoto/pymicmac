7b770ea89dba89dc3a023bd1377796e5a07f7d41
Oscar Martinez Rubi <omrubi@gmail.com>
2016-05-04 10:09:54 +0200

    Add check if MicMac and noodles are installed

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 from distutils.core import setup
+import subprocess, sys
+
+# First we check MicMac in installed and in the PATH
+(out,err) = subprocess.Popen('mm3d -help', shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+if out.decode(sys.stdout.encoding).count('MicMac') == 0:
+    print('Installation could not be done: MicMac (mm3d) could not be found.')
+    sys.exit(1)
+
+# Second we see if noodles has been manually installed and is in PYTHONPATH
+try:
+    import noodles
+except:
+    print('Installation could not be done: noodles could not be found.')
+    sys.exit(1)
 
 setup(
     name='PyMicMac',
