75343f1afde37f609ea37a58882f4f55bb96f98d
Oscar Martinez Rubi <orubi@fs3.cm.cluster>
2016-05-13 12:10:52 +0200

    can specify output folder

--- pymicmac/other/join_homol.py
+++ pymicmac/other/join_homol.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python
 import argparse, os
+from pymicmac import utils_execution
 
-def run(inputFolder):
+def run(inputFolder, outputFolder):
     # Run Tapioca command
-    if os.path.isdir('Homol'):
-        raise Exception('Homol already exists!')
-    os.makedirs('Homol')
+    if os.path.isdir(outputFolder):
+        raise Exception(outputFolder + ' already exists!')
+    os.makedirs(outputFolder)
 
     for e in os.listdir(inputFolder):
-        os.system('cp -r ' + inputFolder + '/' + e + '/* ' + 'Homol' )
+        if os.path.isdir(inputFolder + '/' + e):
+            os.system('cp -r ' + inputFolder + '/' + e + '/* ' + outputFolder)
 
 def argument_parser():
    # define argument menu
     description = ""
     parser = argparse.ArgumentParser(description=description)
     # fill argument groups
-    parser.add_argument('--inputFolder',default='', help='A folder containing all the homol folders to be combined', type=str, required=True)
+    parser.add_argument('-i','--inputFolder',default='', help='A folder containing all the homol folders to be combined', type=str, required=True)
+    parser.add_argument('-o','--outputFolder',default='', help='Output folder', type=str, required=True)
     return parser
 
 if __name__ == "__main__":
     try:
         a = utils_execution.apply_argument_parser(argument_parser())
-        run(a.inputFolder)
+        run(a.inputFolder, a.outputFolder)
     except Exception as e:
         print(e)
