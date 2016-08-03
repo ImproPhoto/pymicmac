b3c5ebf3a6292a559c22a5c861c79ab57f5cfe0f
Oscar Martinez Rubi <omrubi@gmail.com>
2016-08-03 18:25:45 +0200

    uncomment tests

--- tests/run_workflow_test.sh
+++ tests/run_workflow_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# micmac-run-workflow -d . -e tie-point-detection -c tie-point-detection.xml
-# micmac-run-workflow -d . -e param-estimation -c param-estimation.xml
-# micmac-run-workflow -d . -e param-estimation-red -c param-estimation_reduction.xml
-# micmac-run-workflow -d . -e param-estimation-orired -c param-estimation_orireduction.xml
+micmac-run-workflow -d . -e tie-point-detection -c tie-point-detection.xml
+micmac-run-workflow -d . -e param-estimation -c param-estimation.xml
+micmac-run-workflow -d . -e param-estimation-red -c param-estimation_reduction.xml
+micmac-run-workflow -d . -e param-estimation-orired -c param-estimation_orireduction.xml
 micmac-run-workflow -d . -e matching -c matching.xml
