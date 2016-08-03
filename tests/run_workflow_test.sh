2997910a2d1c8973e55f813aed9fa8a6b4450964
Oscar Martinez Rubi <omrubi@gmail.com>
2016-08-03 18:23:38 +0200

    Add MicMac-LocalChantierDescripteur.xml in test folder to avoid having to add stuff in MicMac install

--- tests/run_workflow_test.sh
+++ tests/run_workflow_test.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-#
-# Before ruuning the test, add the following in include/XML_User/DicoCamera.xml:
-# <CameraEntry>
-#      <Name> Aquaris E5 HD </Name>
-#      <SzCaptMm> 3.52 4.69 </SzCaptMm>
-#      <ShortName> Aquaris E5 HD </ShortName>
-# </CameraEntry>
-
-micmac-run-workflow -d . -e tie-point-detection -c tie-point-detection.xml
-micmac-run-workflow -d . -e param-estimation -c param-estimation.xml
-micmac-run-workflow -d . -e param-estimation-red -c param-estimation_reduction.xml
-micmac-run-workflow -d . -e param-estimation-orired -c param-estimation_orireduction.xml
+# micmac-run-workflow -d . -e tie-point-detection -c tie-point-detection.xml
+# micmac-run-workflow -d . -e param-estimation -c param-estimation.xml
+# micmac-run-workflow -d . -e param-estimation-red -c param-estimation_reduction.xml
+# micmac-run-workflow -d . -e param-estimation-orired -c param-estimation_orireduction.xml
 micmac-run-workflow -d . -e matching -c matching.xml
