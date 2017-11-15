b4b29520b7ff95b143af9ef38920a63f4130087b
cwmeijer <c.meijer@esciencecenter.nl>
2017-11-15 13:33:17 +0100

    stop test script if intermediat step crashes and check for dependencies first

--- tests/run_workflow_test.sh
+++ tests/run_workflow_test.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+set -e
+echo "Checking dependencies..."
+echo "ImageMagick:"
+convert --version
+echo "exiv2:"
+exiv2 --version
+echo "exiftool:"
+exiftool --version
+
+echo "micmac-run-workflow -d . -e tie-point-detection -c tie-point-detection.xml"
 micmac-run-workflow -d . -e tie-point-detection -c tie-point-detection.xml
+echo "micmac-run-workflow -d . -e param-estimation -c param-estimation.xml"
 micmac-run-workflow -d . -e param-estimation -c param-estimation.xml
+echo "micmac-run-workflow -d . -e param-estimation-red -c param-estimation_reduction.xml"
 micmac-run-workflow -d . -e param-estimation-red -c param-estimation_reduction.xml
+echo "micmac-run-workflow -d . -e param-estimation-orired -c param-estimation_orireduction.xml"
 micmac-run-workflow -d . -e param-estimation-orired -c param-estimation_orireduction.xml
+echo "micmac-run-workflow -d . -e matching -c matching.xml"
 micmac-run-workflow -d . -e matching -c matching.xml
+echo "done."
