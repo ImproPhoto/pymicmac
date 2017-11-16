ccad44cd8cff00a7da1b7b0d9bbc8cf5c8a09530
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2017-11-16 11:16:27 +0100

    Do not run RedTiep.

--- tests/run_workflow_test.sh
+++ tests/run_workflow_test.sh
@@ -12,10 +12,10 @@ echo "micmac-run-workflow -d . -e tie-point-detection -c tie-point-detection.xml
 micmac-run-workflow -d . -e tie-point-detection -c tie-point-detection.xml
 echo "micmac-run-workflow -d . -e param-estimation -c param-estimation.xml"
 micmac-run-workflow -d . -e param-estimation -c param-estimation.xml
-echo "micmac-run-workflow -d . -e param-estimation-red -c param-estimation_reduction.xml"
-micmac-run-workflow -d . -e param-estimation-red -c param-estimation_reduction.xml
-echo "micmac-run-workflow -d . -e param-estimation-orired -c param-estimation_orireduction.xml"
-micmac-run-workflow -d . -e param-estimation-orired -c param-estimation_orireduction.xml
+#echo "micmac-run-workflow -d . -e param-estimation-red -c param-estimation_reduction.xml"
+#micmac-run-workflow -d . -e param-estimation-red -c param-estimation_reduction.xml
+#echo "micmac-run-workflow -d . -e param-estimation-orired -c param-estimation_orireduction.xml"
+#micmac-run-workflow -d . -e param-estimation-orired -c param-estimation_orireduction.xml
 echo "micmac-run-workflow -d . -e matching -c matching.xml"
 micmac-run-workflow -d . -e matching -c matching.xml
 echo "done."
